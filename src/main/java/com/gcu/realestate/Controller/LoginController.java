e068c036a40a6c58695f29a0acc33b300d0ad613
samson <samsonbecenti@gmail.com>
2023-02-25 00:08:30 -0700

    Created Login Page

--- src/main/java/com/gcu/realestate/Controller/LoginController.java
+++ src/main/java/com/gcu/realestate/Controller/LoginController.java
@@ -2,6 +2,7 @@ package com.gcu.realestate.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,11 +24,36 @@ public class LoginController {
     }
 
     @PostMapping("/doLogin")
-    public String doLogin(LoginModel loginModel, Model model) {
+    public String doLogin(LoginModel loginModel, BindingResult bindingResult, Model model) {
 
-        model.addAttribute("loginModel", loginModel);
+        // Check for validation erros
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("loginModel", loginModel);
 
-        return "LoginSuccess";
+            return "LoginSuccess";
+        }
+
+        // Validate Logins
+        String[][]validLogins = new String[][] {
+            {"gcu", "1234"},
+           {"username", "password"}
+         };
+
+        // Check if login is valid
+        boolean success = false;
+        for(int i = 0; i < validLogins.length; i++) {
+          if(loginModel.getUsername().equals(validLogins[i][0]) && loginModel.getPassword().equals(validLogins[i][1])) {
+                success = true;
+            }
+        }
+
+        if (success) {
+            model.addAttribute("loginModel", loginModel);
+            return "LoginSuccess";
+        }
+        else {
+          return "login";
+        }
+
     }
-
 }
--- src/main/java/com/gcu/realestate/Model/LoginModel.java
+++ src/main/java/com/gcu/realestate/Model/LoginModel.java
@@ -4,6 +4,7 @@ public class LoginModel {
     
     private String username;
     private String password;
+    
     public String getUsername() {
         return username;
     }
