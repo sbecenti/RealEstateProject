cf6f083d9da3c599525d1317c5d1dea9563c15e1
SBecenti02 <sbecenti@my.gcu.edu>
2023-02-24 17:34:42 -0700

    Started Creating Registration Controller

--- src/main/java/com/gcu/realestate/Controller/RegistrationController.java
+++ src/main/java/com/gcu/realestate/Controller/RegistrationController.java
@@ -4,30 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
 
 import com.gcu.realestate.Model.LoginModel;
 
 
 @Controller
-@RequestMapping("/login") 
-public class LoginController {
+@RequestMapping("/register")
+public class RegistrationController {
     
     @GetMapping("/")
     public String display(Model model) {
         
-        model.addAttribute("title", "Login Form");
+        model.addAttribute("title", "Sign Up");
         model.addAttribute("loginModel", new LoginModel());
 
-        return "login";
-    }
-
-    @PostMapping("/doLogin")
-    public String doLogin(LoginModel loginModel, Model model) {
-
-        model.addAttribute("loginModel", loginModel);
-
-        return "LoginSuccess";
+        return "registration";
     }
 
 }
